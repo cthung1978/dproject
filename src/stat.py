a18a9c53b81e8628b84df761493aa5ffabf59b49
CTHung <cthung1978@gmail.com>
2021-04-17 12:42:51 +0800

    update

--- src/stat.py
+++ src/stat.py
@@ -30,7 +30,7 @@ watch = data[np.size(data, 0)-1]
 # 整理 rawdata[U-1][D-1][0] 排名
 # 整理 rawdata[U-1][D-1][1] 統計
 if staticsoutputfilename != '':
-	outputfile = open(staticsoutputfilename, mode='w')
+    outputfile = open(staticsoutputfilename, mode='w')
 rawdata = np.zeros(jump * 7 * 2 * 50, dtype='i8')
 rawdata = rawdata.reshape(jump, 7, 2, 50)
 for U in range (1, jump+1):
@@ -62,6 +62,17 @@ for U in range (1, jump+1):
 if staticsoutputfilename != '':
     outputfile.close()
 
+
+### 檔案輸出
+## 輸出 === [乙4 完成 列印 "號碼" ] == " V4_01_target" → " D1_2_3_5 " ======
+# flag = 1 表示輸出檔案 其他數字(-1, 0, 2, 3, 4..) 則不輸出
+flag=1
+if flag > 0:
+    left_order_out_file = open('left_order.txt', mode='at')
+    right_order_out_file = open('right_order.txt', mode='at')
+    left_statics_out_file = open('left_statics.txt', mode='at')
+    right_statics_out_file = open('right_statics.txt', mode='at')
+
 ### Distribution 取代原本的 D_1_2_3_4
 for d in combinations([1, 2, 3, 4, 5, 6, 7], combo_number):
     # print d
@@ -73,101 +84,98 @@ for d in combinations([1, 2, 3, 4, 5, 6, 7], combo_number):
     ### 函式寫在 filter.py 取代原本 乙一
     ### 統計組數無上限 設定定義在 filter.txt
     countdata = filter.load_filter(target_Dw_x_y_z)
-    # print countdata
+    linenumber = len(countdata)
 
     (L_coef, R_coef) = filter.load_subfilter(target_Dw_x_y_z)
     #print L_coef
     #print R_coef
 
     ### 原本的乙四
-    left_countdata = np.zeros(49 * 2 * 50, dtype='i8')
-    left_countdata = left_countdata.reshape(49, 2, 50)
-    for line in range(49):
+    left_countdata = np.zeros(linenumber * 2 * 50, dtype='i8')
+    left_countdata = left_countdata.reshape(linenumber, 2, 50)
+    for line in range(linenumber):
         for col in range(1, 50):
             left_countdata[line][1][col] = countdata[line][1][col] + L_coef[col]
         left_countdata[line][0] = np.argsort(-left_countdata[line][1])
 
-    right_countdata = np.zeros(49 * 2 * 50, dtype='i8')
-    right_countdata = right_countdata.reshape(49, 2, 50)
+    right_countdata = np.zeros(linenumber * 2 * 50, dtype='i8')
+    right_countdata = right_countdata.reshape(linenumber, 2, 50)
 
-    for line in range(49):
+    for line in range(linenumber):
         for col in range(1, 50):
             right_countdata[line][1][col] = countdata[line][1][50-col] + R_coef[col]
         right_countdata[line][0] = np.argsort(-right_countdata[line][1])
     ### 原本的乙四結束
 
 
-    ### 檔案輸出
-    ## 輸出 === [乙4 完成 列印 "號碼" ] == " V4_01_target" → " D1_2_3_5 " ======
-    # flag = 1 表示輸出檔案 其他數字(-1, 0, 2, 3, 4..) 則不輸出
-    flag=1
-    if flag == 1:
-        left_out_file = open('left_order.txt', mode='at')
-        right_out_file = open('right_order.txt', mode='at')
+    if flag > 0:
+        if flag == 1:
+            outstring = 'D_w_x_y_z\t\t'
+            for col in range(1, 50):
+                outstring = outstring + str(col) + '\t'
+            outstring = outstring + '\n'
+            left_order_out_file.write(outstring)
 
-        # outstring = '\t\t'
-        # for col in range(1, 50):
-        #     outstring = outstring + str(col) + '\t'
-        # outstring = outstring + '\n'
-        # left_out_file.write(outstring)
-
-        for line in range(49):
+        for line in range(linenumber):
             outstring = 'D_' + str(target_Dw_x_y_z.n[0]) + '_' + str(target_Dw_x_y_z.n[1]) + '_' + str(target_Dw_x_y_z.n[2]) + '_' + str(target_Dw_x_y_z.n[3]) + '\t'
             outstring = outstring + str(line+1) + '\t'
             for col in range( 49):
                 outstring = outstring + str(left_countdata[line][0][col]) + '\t'
             outstring = outstring + '\n'
-            left_out_file.write(outstring)
+            left_order_out_file.write(outstring)
 
-        # outstring = '\t\t'
-        # for col in range(49, 0, -1):
-        #     outstring = outstring + str(col) + '\t'
-        # outstring = outstring + '\n'
-        # right_out_file.write(outstring)
+        if flag == 1:
+            outstring = 'D_w_x_y_z\t\t'
+            for col in range(49, 0, -1):
+                outstring = outstring + str(col) + '\t'
+            outstring = outstring + '\n'
+            right_order_out_file.write(outstring)
 
-        for line in range(49):
+        for line in range(linenumber):
             outstring = 'D_' + str(target_Dw_x_y_z.n[0]) + '_' + str(target_Dw_x_y_z.n[1]) + '_' + str(target_Dw_x_y_z.n[2]) + '_' + str(target_Dw_x_y_z.n[3]) + '\t'
             outstring = outstring + str(line+1) + '\t'
             for col in range( 49):
                 outstring = outstring + str(right_countdata[line][0][col]) + '\t'
             outstring = outstring + '\n'
-            right_out_file.write(outstring)
-        left_out_file.close()
-        right_out_file.close()
+            right_order_out_file.write(outstring)
 
-    ## 輸出 ===== [乙4 (丙)區完成 列印 "數量" ] == " V4_01_target_Dw_x_y_z " → " D1_2_3_5 "======
-    # flag = 1 表示輸出檔案 其他數字(-1, 0, 2, 3, 4..) 則不輸出
-    flag=1
-    if flag==1:
-        left_out_file = open('left_statics.txt', mode='at')
-        right_out_file = open('right_statics.txt', mode='at')
-        # outstring = '\t\t'
-        # for col in range(1, 50):
-        #     outstring = outstring + str(col) + '\t'
-        # outstring = outstring + '\n'
-        # left_out_file.write(outstring)
+    if flag > 0:
 
-        for line in range(49):
+        if flag == 1:
+            outstring = 'D_w_x_y_z\t\t'
+            for col in range(1, 50):
+                outstring = outstring + str(col) + '\t'
+            outstring = outstring + '\n'
+            left_statics_out_file.write(outstring)
+
+        for line in range(linenumber):
             outstring = 'D_' + str(target_Dw_x_y_z.n[0]) + '_' + str(target_Dw_x_y_z.n[1]) + '_' + str(target_Dw_x_y_z.n[2]) + '_' + str(target_Dw_x_y_z.n[3]) + '\t'
             outstring = outstring + str(line+1) + '\t'
             for col in range( 1, 50):
                 outstring = outstring + str(left_countdata[line][1][col]) + '\t'
             outstring = outstring + '\n'
-            left_out_file.write(outstring)
+            left_statics_out_file.write(outstring)
 
-        # outstring = '\t\t'
-        # for col in range(49, 0, -1):
-        #     outstring = outstring + str(col) + '\t'
-        # outstring = outstring + '\n'
-        # right_out_file.write(outstring)
+        if flag == 1:
+            flag = 2
+            outstring = 'D_w_x_y_z\t\t'
+            for col in range(49, 0, -1):
+                outstring = outstring + str(col) + '\t'
+            outstring = outstring + '\n'
+            right_statics_out_file.write(outstring)
 
-        for line in range(49):
+        for line in range(linenumber):
             outstring = 'D_' + str(target_Dw_x_y_z.n[0]) + '_' + str(target_Dw_x_y_z.n[1]) + '_' + str(target_Dw_x_y_z.n[2]) + '_' + str(target_Dw_x_y_z.n[3]) + '\t'
             outstring = outstring + str(line+1) + '\t'
             #for col in range(49, 0, -1):
             for col in range( 1, 50):
                 outstring = outstring + str(right_countdata[line][1][col]) + '\t'
             outstring = outstring + '\n'
-            right_out_file.write(outstring)
-        left_out_file.close()
-        right_out_file.close()
+            right_statics_out_file.write(outstring)
+
+
+if flag > 0:
+    left_order_out_file.close()
+    right_order_out_file.close()
+    left_statics_out_file.close()
+    right_statics_out_file.close()
